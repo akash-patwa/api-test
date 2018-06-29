2805a46739dc8ee17a806ba09fbd7164815c11c2
Prasanta Biswas <prasanta.biswas@seamless.se>
2018-06-29 12:40:45 +0530

    added CustomerApp main

---
+++ src/main/java/io/apitest/example/CustomerApp.java
@@ -0,0 +1,25 @@
+package io.apitest.example;
+
+import io.apitest.example.enums.CustomerStatus;
+import io.apitest.example.enums.FieldType;
+import io.apitest.example.model.*;
+import io.apitest.example.repository.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@SpringBootApplication
+public class CustomerApp {
+
+	private static final Logger logger = LoggerFactory.getLogger(CustomerApp.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerApp.class, args);
+	}
+}
