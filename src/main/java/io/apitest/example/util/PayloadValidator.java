96e7c4f94f9d8920892aa250fae2634999915be0
Prasanta Biswas <prasanta.biswas@seamless.se>
2018-06-29 16:48:13 +0530

    update payload validatior

--- src/main/java/io/apitest/example/util/PayloadValidator.java
+++ src/main/java/io/apitest/example/util/PayloadValidator.java
@@ -1,6 +1,11 @@
 package io.apitest.example.util;
 
+import io.apitest.example.interfaces.ViewService;
+import io.apitest.example.interfaces.WorkflowService;
 import io.apitest.example.model.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 /**
@@ -10,9 +15,22 @@ import org.springframework.stereotype.Service;
 @Service
 public class PayloadValidator {
 
+    @Autowired
+    private ViewService viewService;
+
+    @Autowired
+    private WorkflowService workflowService;
+
     public int validateCreateCustomerPayload(Customer customer) {
         if (customer.getId() > 0 || customer.getId() < 0){
             return -1;
+        }
+        else if(viewService.getViewById(customer.getViewId()) == null) {
+            System.out.println("All views: "+viewService.getAllView());
+            return -2;
+        }
+        else if(workflowService.getWorkflowById(customer.getWorkflowId()) == null) {
+            return -3;
         }
         return 0;
     }
