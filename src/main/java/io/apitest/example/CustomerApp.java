cca8868891deeff6ea003663c9572bec4be4990a
Prasanta Biswas <prasanta.biswas@seamless.se>
2018-06-29 13:45:01 +0530

    added field dummy data in database

--- src/main/java/io/apitest/example/CustomerApp.java
+++ src/main/java/io/apitest/example/CustomerApp.java
@@ -21,6 +21,7 @@ import java.util.List;
 @SpringBootApplication
 public class CustomerApp {
 
+
 	private static final Logger logger = LoggerFactory.getLogger(CustomerApp.class);
 
 	public static void main(String[] args) {
@@ -35,6 +36,30 @@ public class CustomerApp {
 			customerRepository.save(new Customer("User 3","Mumbai, Maharastra",true, CustomerStatus.BLOCKED,3,3));
 			customerRepository.save(new Customer("User 4","Chennai, Tamilnadu",false, CustomerStatus.ACTIVE,4,4));
 			logger.info("The sample customer data has been generated");
+		};
+	}
+
+	@Bean
+	public CommandLineRunner setupField(FieldRepository fieldRepository) {
+		return (args) -> {
+			fieldRepository.save(new Field("Name1",FieldType.STRING));
+			fieldRepository.save(new Field("Address1",FieldType.STRING));
+			fieldRepository.save(new Field("Is Onboarded1",FieldType.BOOLEAN));
+			fieldRepository.save(new Field("Status1",FieldType.INTEGER));
+			fieldRepository.save(new Field("Name2",FieldType.STRING));
+			fieldRepository.save(new Field("Address2",FieldType.STRING));
+			fieldRepository.save(new Field("Is Onboarded2",FieldType.BOOLEAN));
+			fieldRepository.save(new Field("Name3",FieldType.STRING));
+			fieldRepository.save(new Field("Address3",FieldType.STRING));
+			fieldRepository.save(new Field("Status3",FieldType.INTEGER));
+			fieldRepository.save(new Field("Name4",FieldType.STRING));
+			fieldRepository.save(new Field("Address4",FieldType.STRING));
+			fieldRepository.save(new Field("Is Onboarded4",FieldType.STRING));
+			fieldRepository.save(new Field("Status14",FieldType.STRING));
+			fieldRepository.save(new Field("Name5",FieldType.STRING));
+			fieldRepository.save(new Field("Address5",FieldType.STRING));
+			fieldRepository.save(new Field("Status5",FieldType.STRING));
+			logger.info("The sample field data have been generated");
 		};
 	}
 }
