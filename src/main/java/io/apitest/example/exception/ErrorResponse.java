8c15eefc05e94f0ea5cfce706767da6bf7949704
Prasanta Biswas <prasanta.biswas@seamless.se>
2018-06-30 16:07:59 +0530

    modified error response class

--- src/main/java/io/apitest/example/exception/ErrorResponse.java
+++ src/main/java/io/apitest/example/exception/ErrorResponse.java
@@ -1,12 +1,37 @@
 package io.apitest.example.exception;
 
+import java.util.Date;
+
 /**
  * Created by prasantabiswas on 29/06/18.
  */
 public class ErrorResponse {
 
+    private Date timestamp;
     private int errorCode;
     private String message;
+    private String details;
+
+    public ErrorResponse()
+    {
+        super();
+    }
+
+    public ErrorResponse(Date timestamp, int errorCode, String message, String details)
+    {
+        this.timestamp = timestamp;
+        this.errorCode = errorCode;
+        this.message = message;
+        this.details = details;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Date timestamp) {
+        this.timestamp = timestamp;
+    }
 
     public int getErrorCode() {
         return errorCode;
@@ -22,5 +47,13 @@ public class ErrorResponse {
 
     public void setMessage(String message) {
         this.message = message;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
     }
 }
